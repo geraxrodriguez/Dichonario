cacb5e66cc0febefe65addbc6df96d655de570d2
gerardo rodriguez <gerarodriguez@proton.me>
2024-10-12 17:04:34 -0500

    refactor: delete some comments

--- frontend/src/pages/DichosPage.jsx
+++ frontend/src/pages/DichosPage.jsx
@@ -16,23 +16,23 @@ const Dichos = () => {
       }
     };
     getDichos();
-  }, []); // ends useEffect()
+  }, []);
 
   return (
     // <section className="bg-blue-50 px-4 py-6 h-5/6 overflow-auto">
     <section className="flex flex-1 justify-center bg-blue-50 px-4 py-6">
 
-        <div className="px-4">
-          <h2 className="text-3xl font-bold mb-6 text-center">
-            Los Dichos
-          </h2>
+      <div className="px-4">
+        <h2 className="text-3xl font-bold mb-6 text-center">
+          Los Dichos
+        </h2>
 
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {dichos.map((dicho) => (
-              <DichoCard key={dicho._id} dicho={dicho} />
-            ))}
-          </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {dichos.map((dicho) => (
+            <DichoCard key={dicho._id} dicho={dicho} />
+          ))}
         </div>
+      </div>
 
     </section>
   )
