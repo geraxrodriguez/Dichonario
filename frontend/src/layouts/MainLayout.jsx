c53a40c5befc9ba759af5effd345597ac3dc5e6e
gerardo rodriguez <gerarodriguez@proton.me>
2024-10-13 12:04:45 -0500

    refactor: cleaned up MainLayout and installed Tailwind prettier plugin

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import styles from '../styles/MainLayout.module.css'
-
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
-    // <div className="h-screen bg-indigo-700 text-center flex flex-col">
-    <div className={styles.layout}>
-        < Navbar />
-        < Outlet />
-        < Footer />
+    <div className="h-screen w-screen overflow-auto bg-indigo-700 flex flex-col text-center">
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
