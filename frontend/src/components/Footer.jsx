2a41de1f7aca0b8c9b500742b6be415d0e173c3b
gerardo rodriguez <gerarodriguez@proton.me>
2024-10-17 02:17:19 -0500

    finished converting navbar to tailwind

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,8 @@ import styles from '../styles/Navbar.module.css'
 
 const Footer = () => {
   return (
-    <div className={styles.navbar}>
+    // <div className={styles.navbar}>
+    <div className="flex flex-col justify-center border-t border-indigo-500 mt-auto min-h-16 text-stone-300">
       <span>Website by: Gerardo Rodriguez</span>
       <span>Pa' Mi Gente | La Lucha Sigue</span>
     </div>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,37 +10,32 @@ const Navbar = () => {
   return (
     <nav className='border-b border-indigo-500'>
 
-      {/* <div className="mx-auto max-w-4xl min-w-96 lg:px-8 sm:px-6 border border-black"> */}
-      <div className={styles.outerContainer}>
+      <div className='mx-auto max-w-5xl min-w-80 md:px-6 lg:px-8'>
 
-        <div className='flex h-20 items-center justify-between'>
-          
+        <div className='flex h-20 items-center'>
+
           <div className="flex flex-1 items-center justify-center  md:items-stretch md:justify-start"> {/* flex-1 items grow and shrink to distribute space evenly */}
 
             <NavLink className="flex items-center mr-2 md:ml-16" to="/"> {/* flex-shrink-0 => item will not shrink */}
-              <span className="hidden md:block text-white text-2xl font-bold"> {/* hidden md:block => hides element until md breakpoint is reached, typically >= 768px */}
+              <span className="hidden md:block text-white text-3xl font-bold"> {/* hidden md:block => hides element until md breakpoint is reached, typically >= 768px */}
                 Dichonario
               </span>
-              {/* <span className="md:hidden w-auto text-white text-4xl font-bold">  */}
-              <span id={styles.dnLetters}> 
+              <span className='w-auto text-white text-4xl font-bold md:hidden '>
                 DN
               </span>
-
             </NavLink>
 
-            {/* <div className="md:ml-auto"> */}
-              <div className={styles.linkContainer}>
-                <NavLink to="/" className={linkClass}>
-                  Home
-                </NavLink>
-                <NavLink to="/dichos" id={styles.dichoLink} className={linkClass}>
-                  Dichos
-                </NavLink>
-                <NavLink to="/submit-dicho" className={linkClass}>
-                  Submit a Dicho
-                </NavLink>
-              </div>
-            {/* </div> */}
+            <div className='flex md:ml-auto'>
+              <NavLink to="/" className={linkClass}>
+                Home
+              </NavLink>
+              <NavLink to="/dichos" id={styles.dichoLink} className={linkClass}>
+                Dichos
+              </NavLink>
+              <NavLink to="/submit-dicho" className={linkClass}>
+                Submit a Dicho
+              </NavLink>
+            </div>
 
           </div>
 
